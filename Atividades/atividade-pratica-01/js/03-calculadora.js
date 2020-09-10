598fffbd4e301ee0918e227d33c349ca8dc4dc7b
Caio Lucas da Silva <clsdraco@gmail.com>
2020-09-10 18:39:47 -0300

    Atualizado Trabalho Prático

---
+++ Atividades/atividade-pratica-01/js/03-calculadora.js
@@ -0,0 +1,53 @@
+var text = '0';
+
+var lcd = null;
+
+window.onload = ()=>{
+    lcd = document.getElementById('lcd');
+    imprime(text);
+}
+
+function adicionaDigito(digito){
+    if(text == '0'){
+        text = ''+digito;
+    } else{
+        text += digito;
+    }
+
+    imprime(text);
+}
+
+function deletaDigito(){
+    text = text.slice(0, -1);
+
+    if(text == ''){
+        text = '0';
+    }
+
+    imprime(text);
+}
+
+function limpaTela(){
+    text = '0';
+
+    imprime(text);
+}
+
+function calcula(){
+    try {
+        text = ''+eval(text);
+        imprime(text);
+    } catch (error) {
+        text = '0';
+        imprime('Invalido');
+    }    
+}
+
+function imprime(texto){
+    if(texto.length > 14){
+        texto = texto.slice(0, 14-text.length);
+        text = texto;
+    }
+
+    lcd.innerText = text;
+}
