0a5acea4e7475ed7474cc57d820fcdc87513bbb4
Caio Lucas da Silva <clsdraco@gmail.com>
2020-10-01 17:44:52 -0300

    Adicionada Aula 22

--- Atividades/exercicios/laravel/routes/web.php
+++ Atividades/exercicios/laravel/routes/web.php
@@ -13,6 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+use App\Models\Produto;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/produtos', function () {
+
+    $produtos = Produto::all();
+    return view('produtos', ['dados'=>$produtos]);
+});
+
+Route::get('/produtos/{id}', function ($id) {
+
+    $produto = Produto::findOrFail($id);
+    return view('produtos', ['dados'=>$produto]);
 });
