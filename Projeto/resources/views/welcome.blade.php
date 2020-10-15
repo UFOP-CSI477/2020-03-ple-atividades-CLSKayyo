b38da9add5ef169b549ad28b4d05f1c649f2c604
Caio Lucas da Silva <clsdraco@gmail.com>
2020-10-15 14:55:31 -0300

    Atualização Trabalho Final

--- Projeto/resources/views/welcome.blade.php
+++ Projeto/resources/views/welcome.blade.php
@@ -1,11 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+@extends('layouts.app')
+
+@section('title')
+    <title>HistorYou</title>
+@endsection
+
+@section('content')
+<link rel="stylesheet" href="{{asset('css/welcome.css')}}">
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1>HistorYou - Crie sua história!</h1>
+            <a href="{{route('login')}}"><button class="btn btn-outline-light">Entrar</button></a>
+            <a href="{{route('register')}}"><button class="btn btn-outline-light">Cadastre-se</button></a>
+            
+        </div>
+    </div>
+</div>
+@endsection
